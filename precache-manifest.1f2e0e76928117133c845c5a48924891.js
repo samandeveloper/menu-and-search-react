ff7b6f25852a1b44edc74b2b71060e84b376209b
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-09 11:56:02 -0700

    Updates

---
+++ precache-manifest.1f2e0e76928117133c845c5a48924891.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704a07d9a1bab1f1e5d30ada943850b4",
+    "url": "/menu-and-search-react/index.html"
+  },
+  {
+    "revision": "474614241cb8edd7aa33",
+    "url": "/menu-and-search-react/static/css/main.470e9f0c.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/menu-and-search-react/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/menu-and-search-react/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474614241cb8edd7aa33",
+    "url": "/menu-and-search-react/static/js/main.60e0bf88.chunk.js"
+  },
+  {
+    "revision": "4f923d748487168be8f8",
+    "url": "/menu-and-search-react/static/js/runtime-main.efeaaaf7.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/menu-and-search-react/static/media/logo.f3ad7664.svg"
+  }
+]);
